eacd42dc9ffac88dd4b0c73b8e5a8fb433e84792
Kyryl Frosyniak <kfrosyniak@interia.pl>
2026-04-04 02:02:30 +0200

    Fixing dependencies

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -3,8 +3,7 @@ module.exports = function (api) {
     return {
         presets: ['babel-preset-expo'],
         plugins: [
-            'react-native-reanimated/plugin',
-            'transform-inline-environment-variables',
+            'react-native-reanimated/plugin'
         ],
     };
 };
